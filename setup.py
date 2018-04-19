f1dc6f88b45cf336d9b8344ac959ada8f2c3cf18
DomiDre <homiedomi@gmail.com>
2018-04-19 11:06:32 +0200

    Set basic package structure

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='model.py',
+    version='1.0.0',
+    author='Dominique Dresen',
+    author_email='dominique.dresen@uni-koeln.de',
+    url='https://github.com/DomiDre/modelpy',
+    description='General purpose package to fit models to data',
+    long_description=readme,
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
