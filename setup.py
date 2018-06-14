ac058c6d1453068556c0994102b8a0718977acef
DomiDre <homiedomi@gmail.com>
2018-06-14 13:29:57 +0200

    Update tabs

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@ from setuptools import setup, find_packages
 
 
 with open('README.md') as f:
-    readme = f.read()
+  readme = f.read()
 
 with open('LICENSE') as f:
-    license = f.read()
+  license = f.read()
 
 setup(
-    name='model.py',
-    version='1.0.0',
-    author='Dominique Dresen',
-    author_email='dominique.dresen@uni-koeln.de',
-    url='https://github.com/DomiDre/modelpy',
-    description='General purpose package to fit models to data',
-    long_description=readme,
-    license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+  name='model.py',
+  version='1.0.0',
+  author='Dominique Dresen',
+  author_email='dominique.dresen@uni-koeln.de',
+  url='https://github.com/DomiDre/modelpy',
+  description='General purpose package to fit models to data',
+  long_description=readme,
+  license=license,
+  packages=find_packages(exclude=('tests', 'docs'))
 )
