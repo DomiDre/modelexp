043c92890e605c7c79c0bd17d2680e01478b8930
DomiDre <homiedomi@gmail.com>
2018-07-31 11:40:25 +0200

    Add models for reflectometry, 5 and 6 stacked spheres

--- modelexp/models/reflectometry/__init__.py
+++ modelexp/models/reflectometry/__init__.py
@@ -1,6 +1,8 @@
 from ._reflModel import ReflectometryModel
 
 from ._sphereCSStacked import SphereCSStacked
+from ._sphereCSStacked5 import SphereCSStacked5
+from ._sphereCSStacked6 import SphereCSStacked6
 
 # decorating functions
 from ._instrumentalResolution import InstrumentalResolution
