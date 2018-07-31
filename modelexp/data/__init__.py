87a0b2fd3f1239f15ea95819cb764329657d4f3c
DomiDre <homiedomi@gmail.com>
2018-07-31 11:44:59 +0200

    Add xys data for fullprof spectra

--- modelexp/data/__init__.py
+++ modelexp/data/__init__.py
@@ -2,6 +2,7 @@ from ._data import  Data
 from ._dataContainer import  DataContainer
 from ._xyData import  XyData
 from ._xyeData import  XyeData
+from ._xysData import  XysData
 from ._xyemData import  XyemData
 
 from ._multiData import MultiData
---
+++ modelexp/data/_xysData.py
@@ -0,0 +1,25 @@
+import numpy as np
+from ._xyeData import XyeData
+
+class XysData(XyeData):
+  def loadFromFile(self, filename):
+    self.filename = filename
+    x = []
+    y = []
+    e = []
+    with open(filename, 'r') as f:
+      for line in f:
+        if line.startswith('!'):
+          continue
+        splitLine = line.strip().split()
+        if len(splitLine) != 3:
+          continue
+        x.append(float(splitLine[0]))
+        y.append(float(splitLine[1]))
+        e.append(float(splitLine[2]))
+
+    sortedArgs = np.argsort(x)
+    x = x[sortedArgs]
+    y = y[sortedArgs]
+    e = e[sortedArgs]
+    self.setData(x, y, e)
