2a0a735304f120f2ad2bd1f634d6252dc7301a7d
DomiDre <homiedomi@gmail.com>
2018-08-01 17:23:20 +0200

    Dev version: 0.3.6

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('LICENSE') as f:
 
 setup(
   name='modelexp',
-  version='0.3.5',
+  version='0.3.6',
   description='General purpose package to fit models to experimental data',
   url='https://github.com/DomiDre/modelexp',
   author='Dominique Dresen',
