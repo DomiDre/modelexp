137ede0a649d8e2e597903a1368d62377bda9591
DomiDre <homiedomi@gmail.com>
2018-08-30 15:38:13 +0200

    Set version to 0.3.9

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('LICENSE') as f:
 
 setup(
   name='modelexp',
-  version='0.3.8',
+  version='0.3.9',
   description='General purpose package to fit models to experimental data',
   url='https://github.com/DomiDre/modelexp',
   author='Dominique Dresen',
